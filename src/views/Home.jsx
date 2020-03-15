a09f70b1df95db3364e8a4be38c649685fe27d54
jesus r <lala@gmail.com>
2020-03-15 09:42:35 -0500

    finicht view Home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,30 @@
 // dependencies
-import React from "react";
+import React, { Fragment} from "react";
+import {Carousel} from 'react-materialize';
 
 function Home() {
     return (
-        <h1>Home</h1>
+        <Fragment>
+            <div className="row flex-center">
+                <h1>Bienvenido</h1>
+            </div>
+            <div className="row flex-center">
+                <Carousel
+                    images={[
+                        'http://xpertgroup.co/wp-content/uploads/2018/11/e-precing-b-420x273.jpg',
+                        'http://xpertgroup.co/wp-content/uploads/2019/11/logo-imperium-420x273.jpg',
+                        'http://xpertgroup.co/wp-content/uploads/2018/11/chatbot-b-420x273.jpg',
+                        'http://xpertgroup.co/wp-content/uploads/2018/11/plano-x-b-420x273.jpg',
+                        'http://xpertgroup.co/wp-content/uploads/2018/11/treem-b-420x273.jpg'
+                    ]}
+                    options={{
+                        numVisible: 5,
+                        padding: 0,
+                        shift: 1
+                    }}
+                />
+            </div>
+        </Fragment>
     );
 }
 
